b45c98b6be969106dd7cdfcd2201cdfb395c25ae
Varich Boonsanong <thapecroth@gmail.com>
2021-05-24 17:26:34 +0700

    1. remove not used statemetn

--- components/VegaComp.js
+++ components/VegaComp.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useMemo } from "react";
+import React, { useEffect, useRef } from "react";
 import * as vega from "vega";
 import * as vl from "vega-lite-api";
 import * as vegaLite from "vega-lite";
