840ca3d700ffe838c5e253845788c019d4711ddd
Varich Boonsanong <thapecroth@gmail.com>
2021-05-24 22:24:43 +0700

    1. update VegaComp for memo

--- components/VegaComp.js
+++ components/VegaComp.js
@@ -5,46 +5,40 @@ import * as vegaLite from "vega-lite";
 import * as vegaTooltip from "vega-tooltip";
 import useStore from "../store/index";
 
-const Veg = (props) => {
-  const Main = React.memo(
-    ({ func, name, width, height, options }) => {
-      const container = useRef();
-      const dataSet = useStore((state) => state.dataset);
-      const vegaOptions = {
-        config: {
-          // Vega-Lite default configuration
-        },
-        init: (view) => {
-          // initialize tooltip handler
-          view.tooltip(new vegaTooltip.Handler().call);
-        },
-        view: {
-          renderer: "canvas",
-        },
-      };
-      vl.register(vega, vegaLite, vegaOptions);
-      useEffect(() => {
-        try {
-          func({ dataSet, options })
-            .render()
-            .then((viewElement) => {
-              if (
-                typeof window !== undefined &&
-                container.current &&
-                container
-              ) {
-                container.current.appendChild(viewElement);
-              }
-            });
-        } catch (err) {
-          console.log(err);
-        }
-      }, [container, container.current]);
+const Veg = React.Memo(
+  ({ func, name, width, height, options }) => {
+    const container = useRef();
+    const dataSet = useStore((state) => state.dataset);
+    const vegaOptions = {
+      config: {
+        // Vega-Lite default configuration
+      },
+      init: (view) => {
+        // initialize tooltip handler
+        view.tooltip(new vegaTooltip.Handler().call);
+      },
+      view: {
+        renderer: "canvas",
+      },
+    };
+    vl.register(vega, vegaLite, vegaOptions);
+    useEffect(() => {
+      try {
+        func({ dataSet, options })
+          .render()
+          .then((viewElement) => {
+            if (typeof window !== undefined && container.current && container) {
+              container.current.appendChild(viewElement);
+            }
+          });
+      } catch (err) {
+        console.log(err);
+      }
+    }, [container, container.current]);
 
-      return <div ref={container} tw="rounded-2xl"></div>;
-    },
-    [props]
-  );
-  return <Main {...props}></Main>;
-};
+    return <div ref={container} tw="rounded-2xl"></div>;
+  },
+  [props]
+);
+
 export default Veg;
