fd8d3b44c8ac41148a721180469c275bbcda85ce
Varich Boonsanong <thapecroth@gmail.com>
2021-05-28 18:16:04 +0700

    1. update scrape script

--- backend/scrape/scrape.py
+++ backend/scrape/scrape.py
@@ -1,18 +1,29 @@
 import pandas as pd
 import requests
+import json
 
 df = pd.read_csv('participants.csv')
 print(df)
 
+radius = 1
+
 url = "http://localhost:8080/api/getTweetLocScore"
 
-payload = {
-    "keyword": "a",
-    "lat": 48.88048,
-    "lng": 2.385939,
-    "distance": 1
-}
-headers = {"Content-Type": "application/json"}
+def get_polar(r):
+    payload = {
+        "keyword": "a",
+        "lat": 48.88048,
+        "lng": 2.385939,
+        "distance": radius
+    }
+    headers = {"Content-Type": "application/json"}
 
-response = requests.request("POST", url, json=payload, headers=headers)
-print(response)
+    response = json.load(requests.request("POST", url, json=payload, headers=headers).text)
+    r['pos'] = response.pos
+    r['neg'] = response.neg
+    r['neu'] = response.neu
+    r['comp'] = response.comp
+    return r
+
+df_polar = df.apply(get_polar)
+df.polar.to_csv(f'polar{radius}.csv')
