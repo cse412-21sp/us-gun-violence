08da3317b12ece109b52ee617527b54995043989
Varich Boonsanong <thapecroth@gmail.com>
2021-06-01 07:55:41 +0700

    add more worker

--- backend/scrape/scrape.py
+++ backend/scrape/scrape.py
@@ -4,7 +4,7 @@ from alive_progress import alive_bar
 import datetime
 from pandarallel import pandarallel
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-pandarallel.initialize(progress_bar=True, verbose=0)
+pandarallel.initialize(progress_bar=True, verbose=0, nb_workers= 10)
 import twint
 from stem.control import Controller
 from stem import Signal
